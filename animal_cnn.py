60905e14c5f07cba974f8307f0e19a135a7f04d3
kameoman <spiderman.935963@gmail.com>
2021-09-16 12:00:13 +0900

    model

--- animal_cnn.py
+++ animal_cnn.py
@@ -10,4 +10,15 @@ image_size = 50
 
 # モデルの作成
 def main():
-  x_train, x_test, y_train, y_test = np.load("./")
+  X_train, X_test, y_train, y_test = np.load("./animal.npy")
+  # データを256の範囲から、0~1に正規化し精度を上げる
+  X_train = X_train.astype("float") / 256
+  X_test = X_test.astype("float") / 256
+  # 0と1で動物の種類を再度設定[0-1-0][1-0-0][0-0-1]にして動物を判定する
+  y_train = np_utils.to_categorical(y_train, num_classes)
+  y_test = np_utils.to_categorical(y_test, num_classes)
+
+  # モデル（分類器）の作成
+  model = model_train(X_train, y_train)
+  # モデル（分類器）の評価（テストする）
+  model_eval(model, X_test, y_test)
