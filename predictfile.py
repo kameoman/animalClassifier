dbd157e08b6f0027b4c4d279b313423497d97ea1
kameoman <spiderman.935963@gmail.com>
2021-09-20 20:42:14 +0900

    Flask-model-up

--- predictfile.py
+++ predictfile.py
@@ -1,6 +1,14 @@
 import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from keras.models import Sequential,load_model
+import numpy as np
+from PIL import Image
+import tensorflow
+
+classes = ["monkey","boar","crow"]
+num_classes = len(classes)
+image_size = 50
 
 
 UPLOAD_FOLDER = './uploads'
@@ -29,7 +37,27 @@ def upload_file():
           # 変な文字や危険な文字が無いかを判定
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+            # 判別器を使用して判定結果を出す
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            model = load_model('./animal_cnn_aug.h5')
+
+            image = Image.open(filepath)
+            # 白黒の写真の場合もあるため3色へ変換する
+            image = image.convert('RGB')
+            image = image.resize((image_size,image_size))
+            # 画像を数字の列として保存する
+            data = np.asarray(image)
+            X = []
+            X.append(data)
+            X = np.array(X)
+
+
+
+
+
+
+            # return redirect(url_for('uploaded_file', filename=filename))
     return '''
     <!doctype html>
     <html><head>
@@ -44,3 +72,9 @@ def upload_file():
     </body>
     </html>
     '''
+
+from flask import send_from_directory
+
+@app.route('/upload/<filename>')
+def uploaded_file(filename):
+  return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
